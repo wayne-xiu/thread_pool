cacc53f820702fff8836471813cc952aa906a595
Wenwu Xiu <wenwuxiu1106@gmail.com>
2023-12-10 11:21:33 -0500

    extract lambda in example for easier understanding

--- example/example.cpp
+++ example/example.cpp
@@ -11,13 +11,14 @@ int main()
     std::vector< std::future<int> > results;
 
     for(int i = 0; i < 8; ++i) {
+        auto task = [i] {
+            std::cout << "hello " << i << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            std::cout << "world " << i << std::endl;
+            return i*i;
+        };
         results.emplace_back(
-            pool.enqueue([i] {
-                std::cout << "hello " << i << std::endl;
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-                std::cout << "world " << i << std::endl;
-                return i*i;
-            })
+            pool.enqueue(task)
         );
     }
 
